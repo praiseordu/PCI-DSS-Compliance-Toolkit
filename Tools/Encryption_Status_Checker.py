221a5fbf2377212facc6b46581661b4460ca1087
CyberPraise <64874415+praiseordu@users.noreply.github.com>
2023-12-13 22:38:35 +0100

    Update Encryption_Status_Checker.py

--- Tools/Encryption_Status_Checker.py
+++ Tools/Encryption_Status_Checker.py
@@ -1,16 +1,26 @@
 # Encryption_Status_Checker.py
+import subprocess
 
 def check_disk_encryption_status(device_path):
-    # Simulate checking the encryption status of a disk
     if is_disk_encrypted(device_path):
         print(f"The disk {device_path} is encrypted.")
     else:
         print(f"Warning: The disk {device_path} is not encrypted!")
 
 def is_disk_encrypted(device_path):
-    # Simulate checking if the disk is encrypted
-    # Add actual logic for checking disk encryption status
-    return True
+    try:
+        # Example: Use the 'lsblk' command to check if the disk is encrypted
+        output = subprocess.check_output(['lsblk', '-o', 'NAME,TYPE,CRYPT'])
+        output_lines = output.decode('utf-8').split('\n')
+
+        for line in output_lines:
+            if device_path in line and 'crypt' in line:
+                return True
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+
+    return False
 
 def main():
     device_path = input("Enter the path of the device to check for encryption status: ")
